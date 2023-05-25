d7b9bc72e986f502a73e3839d0b9e6f3f87d2684
Madhusri J <srimadhu.j@gmail.com>
2023-05-25 12:09:16 +0530

    ADD. Security Static

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-//Initial Script
+const form = document.getElementById('myForm');
+const input = document.getElementById('myInput');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location.href = '/src/home.html';
+  });
+  
