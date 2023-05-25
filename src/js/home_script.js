c153b1facbccd378ad51d644a242540a43e85817
Madhusri J <srimadhu.j@gmail.com>
2023-05-25 22:48:08 +0530

    CREATE. Necessary files for stone page

--- src/js/home_script.js
+++ src/js/home_script.js
@@ -1,7 +1,30 @@
-const gauntletElement = document.querySelector('.gauntlet');
+// const gauntletElement = document.querySelector('.gauntlet');
 
-gauntletElement.addEventListener('animationend', () => {
-    console.log("Hey");
-    gauntletElement.remove();
+// gauntletElement.addEventListener('animationend', () => {
+//     gauntletElement.remove();
+// });
+
+const stone = document.getElementById('stone');
+const thanos = document.getElementById('thanos');
+const avengers = document.getElementById('avengers');
+const map = document.getElementById('map');
+
+stone.addEventListener('click',(e) => {
+    e.preventDefault();
+    window.location.href = './stone.html';
 });
 
+thanos.addEventListener('click',(e) => {
+    e.preventDefault();
+    window.location.href = './thanos.html';
+});
+
+avengers.addEventListener('click',(e) => {
+    e.preventDefault();
+    window.location.href = './avengers.html';
+});
+
+map.addEventListener('click',(e) => {
+    e.preventDefault();
+    window.location.href = './map.html';
+});
