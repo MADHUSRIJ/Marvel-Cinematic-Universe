7db066abbb0348e5556192d627c7f4900e060232
Madhusri J <srimadhu.j@gmail.com>
2023-05-25 20:54:00 +0530

    ADD. Home

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,19 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     window.location.href = './src/home.html';
   });
+
+  var contactContainer = document.getElementById("contact-container");
+
+  contactContainer.addEventListener("keydown", function(event) {
+    var currentScrollPosition = contactContainer.scrollTop;
+    
+    if (event.key === "ArrowDown") {
+      contactContainer.scrollTop += 100; 
+      event.preventDefault(); 
+    } else if (event.key === "ArrowUp") {
+      contactContainer.scrollTop -= 100; 
+      event.preventDefault(); 
+    }
+  });
+  
   
