20d153a2ced02b484fe0fd09ca65170cb07e9d07
Madhusri J <srimadhu.j@gmail.com>
2023-05-26 01:23:11 +0530

    UPDATE. stone content

--- src/js/stone_script.js
+++ src/js/stone_script.js
@@ -1,1 +1,55 @@
-//Initial stone script.js
+const space = document.getElementById('space');
+const mind = document.getElementById('mind');
+const reality = document.getElementById('reality');
+const power = document.getElementById('power');
+const time = document.getElementById('time');
+const soul = document.getElementById('soul');
+
+function display(stoneName){
+    var stone = document.getElementById(stoneName);
+    stone.style.transform = 'rotateY(180deg)';
+    stone.style.backgroundColor = 'rgba(224, 120, 120, 0.621)';
+
+    var stone_back = document.getElementById(stoneName+' stone-back')
+    stone_back.classList.remove('hidden');
+
+    var stone_front = document.getElementById(stoneName+' stone-front');
+    stone_front.classList.add('hidden');
+}
+
+function hide(stoneName){
+    var stone = document.getElementById(stoneName);
+    stone.style.transform = 'rotateY(0deg)';
+    stone.style.backgroundColor = 'rgba(240, 248, 255, 0.621)';
+
+    var stone_back = document.getElementById(stoneName+' stone-back')
+    stone_back.classList.add('hidden');
+
+    var stone_front = document.getElementById(stoneName+' stone-front');
+    stone_front.classList.remove('hidden');
+}
+
+space.addEventListener('mouseenter',() =>  display('space'));
+space.addEventListener('mouseleave',() => hide('space'));
+
+time.addEventListener('mouseenter',() =>  display('time'));
+time.addEventListener('mouseleave',() => hide('time'));
+
+reality.addEventListener('mouseenter',() =>  display('reality'));
+reality.addEventListener('mouseleave',() => hide('reality'));
+
+power.addEventListener('mouseenter',() =>  display('power'));
+power.addEventListener('mouseleave',() => hide('power'));
+
+mind.addEventListener('mouseenter',() =>  display('mind'));
+mind.addEventListener('mouseleave',() => hide('mind'));
+
+soul.addEventListener('mouseenter',() =>  display('soul'));
+soul.addEventListener('mouseleave',() => hide('soul'));
+
+const back = document.getElementById('back');
+
+back.addEventListener('click',(e) => {
+    e.preventDefault();
+    window.location.href = './home.html';
+});
